14dd1bec6c3b0c108f66f366b24004b14b9d3e32
Lenoir Christopher <lenoirc.fr@gmail.com>
2020-10-26 22:29:51 +0100

    added sounds for docs

--- docs/website/pages/en/examples.js
+++ docs/website/pages/en/examples.js
@@ -5,15 +5,21 @@ const CompLibrary = require('../../core/CompLibrary.js');
 const Container = CompLibrary.Container;
 const GridBlock = CompLibrary.GridBlock;
 
-function HelloWorld(props) {
-  return (
-    <div className='docMainWrapper wrapper'>
-      <Container className='mainContainer documentContainer postContainer'>
-        <h1>Examples</h1>
-        <p>Here are some examples</p>
-      </Container>
-    </div>
-  );
+class Examples extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className='docMainWrapper wrapper'>
+        <Container className='mainContainer documentContainer postContainer'>
+          <h1>Examples</h1>
+          <p>Here are some examples</p>
+        </Container>
+      </div>
+    );
+  }
 }
 
-module.exports = HelloWorld;
+module.exports = Examples;
